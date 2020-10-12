e61ce9c08763a6c26cd505fd251fdf1092e55605
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-10-12 11:46:20 -0700

    R6 class and leafletProxy: empty map bug fixed

--- allSites/webapp.R
+++ allSites/webapp.R
@@ -1,6 +1,6 @@
+library(R6)
 library(shiny)
 library(leaflet)
-#library(plotKML)
 #----------------------------------------------------------------------------------------------------
 printf <- function(...) print(noquote(sprintf(...)))
 #----------------------------------------------------------------------------------------------------
@@ -10,144 +10,165 @@ config.file <- "/appData/site.yaml"               # for site.yaml loaded in dock
 if(Sys.info()[["sysname"]] == "Darwin")
   config.file <- "site.yaml"                       # for local use, no docker
 
-config <-  yaml.load(readLines(config.file))
-
-points <- config$points
-tbl <- do.call(rbind, lapply(points, as.data.frame))
-tbl$name <- as.character(tbl$name)
-tbl$details <- as.character(tbl$details)
-tbl$id <- as.character(tbl$id)
-# center.lat <- min(tbl$lat) + ((max(tbl$lat) - min(tbl$lat))/2)
-# center.lon <- min(tbl$lon) + ((max(tbl$lon) - min(tbl$lon))/2)
-tbl$label <- tbl$name; # with(tbl, sprintf("%s %s (%s)", name, date, observer))
-markerCategories <- sort(unique(tbl$group))
 #----------------------------------------------------------------------------------------------------
-ui <- fluidPage(
+MapApp = R6Class("MapAppClass",
 
-  titlePanel(config$title),
-  sidebarLayout(
-      sidebarPanel(
-         #includeCSS("map.css"),
-         #selectInput("siteSelector", "Select Site", c("-", sort(tbl$label))),
-         checkboxGroupInput("groupsSelector", "Category", choices = markerCategories, selected=markerCategories),
-         div(actionButton("selectAllCategoriesButton", "All"), style="display: inline-block;vertical-align:top; width: 50px;"),
-         div(actionButton("selectNoCategoriesButton", "None"), style="display: inline-block;vertical-align:top; width: 50px;"),
-         br(), br(), br(),
-         actionButton("fullViewButton", "Full Map"),
-         width=2
-         ),
-      mainPanel(
-          tags$style(type = "text/css", "#map {height: calc(100vh - 120px) !important;}"),
-          tabsetPanel(type="tabs", id="mapTabs",
-                      tabPanel(title="Map", value="mapTab", leafletOutput("map")),
-                      tabPanel(title="Site Details", value="siteDetailsTab",
-                               div(id="foo"))),# , includeHTML("emptyDetails.html")))
-         width=10
-         )
-     )
-)
+    #--------------------------------------------------------------------------------
+    private = list(
+        map = NULL,
+        proxy = NULL,
+        tbl = NULL,
+        centerLon = NULL,
+        centerLat = NULL,
+        initialZoom = NULL,
+        markerCategories = NULL,
+
+        readConfiguration = function(filename){
+           config <-  yaml.load(readLines(filename))
+           points <- config$points
+           tbl <- do.call(rbind, lapply(points, as.data.frame))
+           tbl$name <- as.character(tbl$name)
+           tbl$details <- as.character(tbl$details)
+           tbl$id <- as.character(tbl$id)
+           tbl$label <- tbl$name;
+           private$tbl <- tbl
+           private$centerLon <- config$centerLon
+           private$centerLat <- config$centerLat
+           private$initialZoom <- config$initialZoom
+           }
+
+    ), # private
+    #--------------------------------------------------------------------------------
+    public = list(
+
+       initialize = function(){
+          private$readConfiguration(config.file)
+          private$markerCategories <- sort(unique(private$tbl$group))
+          printf("--- markerCategories")
+          print(private$markerCategories)
+
+          private$map <- leaflet()
+          private$map <- setView(private$map, private$centerLon, private$centerLat, zoom=private$initialZoom)
+          options = leafletOptions(minZoom=0, maxZoom=22, maxNativeZoom=18)
+          options.tile <- tileOptions(minZoom=0, maxZoom=22, maxNativeZoom=18)
+          private$map <- addTiles(private$map, options=options.tile)
+
+          private$map <- addScaleBar(private$map)
+          private$map <- with(private$tbl, addCircleMarkers(private$map,
+                                                            lon, lat,
+                                                            label=label,
+                                                            color=color, radius=radius,
+                                                            layerId=name,
+                                                            group=group))
+
+          #private$map <- addCircleMarkers(private$map,
+          #                                lng = runif(10),
+          #                                lat = runif(10),
+          #                                layerId = paste0("marker", 1:10))
+
+          }, # initialize
+       #--------------------------------------------------------------------------------
+       ui = function(){
+          fluidPage(
+             titlePanel(private$config$title),
+             sidebarLayout(
+               sidebarPanel(
+                   checkboxGroupInput("groupsSelector", "Category", choices=private$markerCategories,
+                                      selected=private$markerCategories),
+                   div(actionButton("selectAllCategoriesButton", "All"), style="display: inline-block;vertical-align:top; width: 50px;"),
+                   div(actionButton("selectNoCategoriesButton", "None"), style="display: inline-block;vertical-align:top; width: 50px;"),
+                   br(), br(), br(),
+                   actionButton("fullViewButton", "Full Map"),
+                   width=2
+                   ),
+               mainPanel(
+                  tags$style(type = "text/css", "#sewardMap {height: calc(100vh - 120px) !important;}"),
+                  tabsetPanel(type="tabs", id="mapTabs",
+                              tabPanel(title="Map", value="mapTab", leafletOutput("sewardMap")),
+                              tabPanel(title="Site Details", value="siteDetailsTab",
+                                       div(id="foo"))),
+                  width=10
+                  ) # mainPanel
+               ) # sidebarLayout
+            ) # fluidPage
+          }, # ui
+       #--------------------------------------------------------------------------------
+       old.ui = function(){
+          fluidPage(
+             tags$style(type = "text/css", "#sewardMap {height: calc(100vh - 120px) !important;}"),
+             leafletOutput("sewardMap"),
+             p(),
+             actionButton("recalc", "New points")
+             )
+
+          }, # old.ui
+       #--------------------------------------------------------------------------------
+       server = function(input, output, session){
+
+         output$sewardMap <- renderLeaflet(private$map)
+         private$proxy <- leafletProxy("sewardMap", session)
+
+         observe({
+            req(input$sewardMap_marker_click)
+            event <- input$sewardMap_marker_click
+            printf("--- map_marker_click: %s", event$id)
+            lat <- event$lat
+            lon <- event$lng
+            details.url <- subset(private$tbl, name==event$id)$details
+            if(nchar(details.url) > 20){
+                removeUI("#temporaryDiv")
+                insertUI("#foo", "beforeEnd", div(id="temporaryDiv"))
+                insertUI("#temporaryDiv", "beforeEnd",
+                         div(id="detailsDiv", includeHTML(details.url)))
+                updateTabsetPanel(session, "mapTabs", selected="siteDetailsTab")    # provided by shiny
+                }
+            }) # map
+
+         observeEvent(input$selectAllCategoriesButton, ignoreInit=TRUE, {
+            printf("--- selectAllCategories")
+            updateTabsetPanel(session, "mapTabs", selected="mapTab")
+            updateCheckboxGroupInput(session, "groupsSelector", selected=private$markerCategories)
+            })
+
+         observeEvent(input$selectNoCategoriesButton, ignoreInit=TRUE, {
+            printf("--- selectNoCategories")
+            updateCheckboxGroupInput(session, "groupsSelector", selected=character(0))
+            updateTabsetPanel(session, "mapTabs", selected="mapTab")
+            })
+
+         observeEvent(input$fullViewButton, ignoreInit=TRUE, {
+            printf("--- fullViewButton")
+            updateTabsetPanel(session, "mapTabs", selected="mapTab")
+            setView(private$proxy, lng=private$centerLon, lat=private$centerLat, zoom=private$initialZoom)
+            })
+
+
+         observeEvent(input$groupsSelector, ignoreInit=TRUE, ignoreNULL=FALSE,  {
+            printf("--- group change")
+            current.groups <- input$groupsSelector
+            all.groups <- unique(private$tbl$group)
+            hidden.groups <- setdiff(all.groups, current.groups)
+            hideGroup(private$proxy, hidden.groups)
+            showGroup(private$proxy, current.groups)
+            })
+
+         observe({   # helpful in accurate positioning of markers
+            x <- input$sewardMap_click
+            req(x)
+            printf("--- map_click")
+            printf("lat: %f", x$lat)
+            printf("lon: %f", x$lng)
+            })
+
+
+         } # server
+       ) # public
+    ) # class
 #----------------------------------------------------------------------------------------------------
-server <- function(input, output, session) {
+app <- MapApp$new()
 
-  observe({  # with no reactive values included here, this seems to run only at startup.
-    searchTerm <- session$clientData$url_search
-    printf("-- starting up, search term?")
-    searchTerm <- sub("?", "", searchTerm, fixed=TRUE)
-    print(searchTerm)
-    if(nchar(searchTerm) > 0){
-      tbl.sub <- subset(tbl, id==searchTerm)
-      printf("sites found matching searchTerm: %d", nrow(tbl.sub))
-      if(nrow(tbl.sub) == 1){
-        lat <- tbl.sub$lat
-        lon <- tbl.sub$lon
-        isolate({
-          leafletProxy('map') %>% setView(lon, lat, zoom=19)
-          })
-         } # nrow
-        } # nchar
-    })
-
-  observeEvent(input$fullViewButton, ignoreInit=TRUE, {
-     updateTabsetPanel(session, "mapTabs", selected="mapTab")
-     isolate({
-        leafletProxy('map') %>%
-           setView(lng=config$centerLon, lat=config$centerLat, zoom=config$initialZoom)
-        })
-      })
-
-  observe({
-     req(input$siteSelector)
-     siteID <- input$siteSelector
-     printf("zoom to '%s'", siteID)
-     tbl.sub <- subset(tbl, label==siteID)
-     if(nrow(tbl.sub) == 1){
-        lat <- tbl.sub$lat
-        lon <- tbl.sub$lon
-        isolate({
-          leafletProxy('map') %>%
-              setView(lon, lat, zoom=18)
-          })
-         } # nrow
-    })
-
-  observeEvent(input$selectAllCategoriesButton, ignoreInit=TRUE,{
-     updateTabsetPanel(session, "mapTabs", selected="mapTab")
-     updateCheckboxGroupInput(session, "groupsSelector", selected=markerCategories)
-     })
-
-  observeEvent(input$selectNoCategoriesButton, ignoreInit=TRUE,{
-     updateTabsetPanel(session, "mapTabs", selected="mapTab")
-     updateCheckboxGroupInput(session, "groupsSelector", selected=character(0))
-     })
-
-  observeEvent(input$clientInfoButton, ignoreInit=FALSE,{
-    searchTerm <- session$clientData$url_search
-    printf("-- starting up, search term?")
-    print(searchTerm)
-    })
-
-  observe({
-     x <- input$map_click
-     printf("lat: %f", x$lat)
-     printf("lon: %f", x$lng)
-     })
-
-
-  observe({
-     req(input$map_marker_click)
-     event <- input$map_marker_click
-     print(names(event))
-     print(event)
-     lat <- event$lat
-     lon <- event$lng
-     printf("lat: %f  lon: %f", lat, lon)
-     details.url <- subset(tbl, name==event$id)$details
-     if(nchar(details.url) > 20){
-        print(details.url)
-        removeUI("#temporaryDiv")
-        insertUI("#foo", "beforeEnd", div(id="temporaryDiv"))
-        insertUI("#temporaryDiv", "beforeEnd",
-                 div(id="detailsDiv", includeHTML(details.url)))
-        updateTabsetPanel(session, "mapTabs", selected="siteDetailsTab")    # provided by shiny
-        }
-     })
-
-  output$map <- renderLeaflet({
-     currentGroups <- input$groupsSelector
-     tbl.sub <- subset(tbl, group %in% currentGroups)
-     options = leafletOptions(minZoom=0, maxZoom=22, maxNativeZoom=18)
-     options.tile <- tileOptions(minZoom=0, maxZoom=22, maxNativeZoom=18)
-     map <- leaflet(options) %>%
-         addTiles(options=options.tile) %>%
-         setView(config$centerLon, config$centerLat, zoom=config$initialZoom) %>%
-         addScaleBar()
-     if(nrow(tbl.sub) > 0)
-         map <- addCircleMarkers(map, tbl.sub$lon, tbl.sub$lat, label=tbl.sub$label,
-                                 color=tbl.sub$color, radius=tbl.sub$radius,
-                                 layerId=tbl.sub$name)
-     map
-     })
-}
-#----------------------------------------------------------------------------------------------------
-with(config, printf("config:  %f   %f   %d", centerLon, centerLat, initialZoom))
-runApp(shinyApp(ui, server), host="0.0.0.0", port=6789)
+if(grepl("hagfish", Sys.info()[["nodename"]])){
+   runApp(shinyApp(app$ui(), app$server), port=1123)
+   } else {
+   shinyApp(app$ui(), app$server)
+   }
