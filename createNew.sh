01adb5a59e6d155886430b7113b12f081bc039e8
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-03-06 15:49:08 -0800

    7 new trees, including 2 cedars

---
+++ createNew.sh
@@ -0,0 +1,25 @@
+SITE=dougFir.109
+mkdir $SITE
+scp paulshannnon@pshannon.net:public_html/pshannon.net/sewardParkRestoration/siteTemplate/* $SITE
+
+SITE=dougFir.110
+mkdir $SITE
+scp paulshannnon@pshannon.net:public_html/pshannon.net/sewardParkRestoration/siteTemplate/* $SITE
+
+SITE=dougFir.111
+mkdir $SITE
+scp paulshannnon@pshannon.net:public_html/pshannon.net/sewardParkRestoration/siteTemplate/* $SITE
+
+SITE=dougFir.112
+mkdir $SITE
+scp paulshannnon@pshannon.net:public_html/pshannon.net/sewardParkRestoration/siteTemplate/* $SITE
+
+SITE=cedar.113
+mkdir $SITE
+scp paulshannnon@pshannon.net:public_html/pshannon.net/sewardParkRestoration/siteTemplate/* $SITE
+
+
+SITE=cedar.114
+mkdir $SITE
+scp paulshannnon@pshannon.net:public_html/pshannon.net/sewardParkRestoration/siteTemplate/* $SITE
+
