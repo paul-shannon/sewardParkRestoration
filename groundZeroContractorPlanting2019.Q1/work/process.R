8e7d0878a33b247493d8b22161f7490963098c55
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-05-22 07:09:13 -0700

    now ready for ongoing record keeping

---
+++ groundZeroContractorPlanting2019.Q1/work/process.R
@@ -0,0 +1,6 @@
+tbl <- read.table("table-raw.tsv", sep="\t", header=TRUE, as.is=TRUE)
+tbl$common <- rownames(tbl)
+rownames(tbl) <- NULL
+coi <- c("binomial", "common", "symbol", "planted", "obs.2020.05.16")
+tbl <- tbl[, coi]
+write.table(tbl, file="record.tsv", sep="\t", quote=FALSE, row.names=FALSE)
