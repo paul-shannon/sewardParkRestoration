0fd583de8fb99eefdfb151e3b4357aa2b8e5e993
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-08-01 09:36:30 -0700

    colors and radii seem good

--- hemlocks/toSiteYaml.R
+++ hemlocks/toSiteYaml.R
@@ -13,7 +13,13 @@
 toStandardForm <- function(tbl.in)
 {
     tbl.in$group <- "Hemlock Decline"
-    tbl.in$color <- "black"
+    health <- rowSums(tbl[, c("dmr1", "dmr2", "dmr3")])
+    colors <-  colorRampPalette(c("green", "black"))(7)  # "#00FF00" "#00D400" "#00AA00" "#007F00" "#005500" "#002A00" "#000000"
+    missing.colors <- which(health < 0)
+    health[health < 0] <- 0
+    color.values <- colors[health+1]
+    tbl.in$color <- color.values
+    tbl.in$color[missing.colors] <- "red"
     tbl.in$details <- "http://pshannon.net/sewardParkRestoration/hemlocks"
 
     coi <- c("id", "observer", "gpsID", "group", "date", "lat", "lon", "color", "dbh", "details")
@@ -22,7 +28,7 @@ toStandardForm <- function(tbl.in)
     colnames(tbl.out)[3] <- "id"
     colnames(tbl.out)[9] <- "radius"
 
-    radii <- round(sqrt(tbl$dbh)) - 1
+    radii <- round(sqrt(tbl$dbh)) # - 1
     radii[radii==0] <- 1
     tbl.out$radius <- radii
     tbl.out$id <- sprintf("waypoint.%s", tbl.out$id)
